587981891aba1ebbdafb13da78e1144d3230a98c
phette23 <phette23@gmail.com>
2024-10-07 15:18:50 -0700

    feat: field conditional on resource type
    ref #8

--- assets/templates/custom_fields/ConditionalField.js
+++ assets/templates/custom_fields/ConditionalField.js
@@ -1,49 +1,33 @@
-import React, {Component} from "react"
-import {TextField} from "react-invenio-forms"
+import { useFormikContext } from "formik"
+import React, { useState, useEffect } from "react"
+import { TextField } from "react-invenio-forms"
 
-// Input only renders if the Invenio record is of a certain resource type
-export class ConditionalField extends Component {
-    constructor(props) {
-        super(props)
-        const resourceType = this.props?.record?.metadata?.resource_type?.id
-        this.state = {
-            active: resourceType ? resourceType.toString().startsWith("publication") : false,
-            interval: setInterval(() => {
-                const resourceType = this.props?.record?.metadata?.resource_type?.id
-                if (this.state.resourceType === resourceType) return
-                this.setState({
-                    active: resourceType ? resourceType.toString().startsWith("publication") : false,
-                    resourceType: resourceType,
-                })
-                this.forceUpdate()
-                clearInterval(this.state.interval)
-            }, 2000),
-            key: this.props.fieldPath + resourceType,
-            resourceType: resourceType,
+const ConditionalField = ({ fieldPath }) => {
+    const {values, setFieldValue} = useFormikContext()
+    const [active, setActive] = useState(false)
+    const [key, setKey] = useState('conditionalfield')
+
+    useEffect(() => {
+        const isActive = values.metadata.resource_type.startsWith("publication")
+        const newKey = `conditionalfield-${values.metadata.resource_type}`
+        if (active !== isActive) {
+            setActive(isActive)
+            // clear the field value when it becomes inactive
+            if (!isActive) setFieldValue(fieldPath, "")
         }
-    }
+        if (key !== newKey) setKey(newKey)
+    }, [values, active, key, fieldPath, setFieldValue])
 
-    componentWillUnmount() {
-        clearInterval(this.state.interval)
-    }
-
-    render() {
-        const {
-            fieldPath, // injected by the custom field loader via the `field` config property
-        } = this.props
-
-        // return text field only for publication types
-        return (
-            <TextField
-                className={this.state.active ? "" : "d-none"}
-                description="Enter the publication title. This field only appears for publication resources."
-                disabled={!this.state.active}
-                fieldPath={fieldPath}
-                key={this.state.key}
-                label="Publication title"
-            />
-        )
-    }
+    return (
+        <TextField
+            className={active ? "" : "d-none"}
+            disabled={!active}
+            fieldPath={fieldPath}
+            helpText="Enter the publication title. This field only appears for publication resources."
+            key={key}
+            label="Publication title"
+        />
+    )
 }
 
 export default ConditionalField
