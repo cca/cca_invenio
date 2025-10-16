e28dbdaaf216adaf762196c11b2585ab1096e7a2
phette23 <phette23@gmail.com>
2025-10-16 16:03:37 -0700

    refactor: reorganize permissions policies
    this also adds a custom `RECORDS_PERMISSIONS_RECORD_POLICY` even though
    it's not clear what that does, it _seems_ like it should control REST
    API access but it has no noticeable effect & does not fix the search
    problem with the last commit

--- site/cca/permissions.py
+++ site/cca/permissions.py
@@ -1,12 +1,13 @@
 from flask_principal import RoleNeed
+from invenio_administration.generators import Administration
+from invenio_communities.permissions import CommunityPermissionPolicy
 from invenio_rdm_records.services.permissions import RDMRecordPermissionPolicy
-from invenio_records_permissions.generators import Generator
+from invenio_records_permissions.generators import Generator, IfConfig, SystemProcess
+from invenio_records_permissions.policies.records import RecordPermissionPolicy
 from invenio_search.engine import dsl
 
-# this turned out to be irrelevant I think, RMD uses RDMRecordPermissionPolicy
-# from invenio_records_permissions.policies.records import RecordPermissionPolicy
 
-
+# Provide role=library access to OBJECT (multiple types) if cca:archives_series exists
 class LibraryStaffIfArchives(Generator):
     def needs(self, record=None, **kwargs):
         print("LibraryStaffIfArchives needs locals", locals())
@@ -26,6 +27,32 @@ class LibraryStaffIfArchives(Generator):
         return dsl.Q("exists", field="custom_fields.cca:archives_series")
 
 
+# Only allow admins to create communities
+# TODO eventually there'll be a Community Creator role we can give only to admins
+# TODO https://github.com/inveniosoftware/rfcs/issues/90
+# https://discord.com/channels/692989811736182844/704625518552547329/1296091404409372763
+class CCACommunityPermissionPolicy(CommunityPermissionPolicy):
+    """Custom community permission policy to only allow admins to create communities."""
+
+    # Override can_create permission. Previous definition:
+    # can_create = [AuthenticatedUser(), SystemProcess()]
+    can_create = [Administration(), SystemProcess()]
+
+    # Since this action uses an action we override, it must also be updated.
+    # Otherwise it would use the `can_create` from the base permission policy.
+    can_create_restricted = [
+        IfConfig("COMMUNITIES_ALLOW_RESTRICTED", then_=can_create, else_=[]),
+    ]
+
+    # without this library staff cannot see records with cca:archives_series fields
+    # in restricted communities that they're not a member of
+    can_read = CommunityPermissionPolicy.can_read + [LibraryStaffIfArchives()]
+
+
+# this turned out to be irrelevant I think, RMD uses RDMRecordPermissionPolicy
+# from invenio_records_permissions.policies.records import RecordPermissionPolicy
+
+
 class CCARDMRecordPermissionPolicy(RDMRecordPermissionPolicy):
     can_read = RDMRecordPermissionPolicy.can_read + [LibraryStaffIfArchives()]
     can_read_files = RDMRecordPermissionPolicy.can_read_files + [
@@ -33,3 +60,8 @@ class CCARDMRecordPermissionPolicy(RDMRecordPermissionPolicy):
     ]
     # can_search = RDMRecordPermissionPolicy.can_search + [LibraryStaffIfArchives()]
     can_view = RDMRecordPermissionPolicy.can_view + [LibraryStaffIfArchives()]
+
+
+class CCARecordPermissionPolicy(RecordPermissionPolicy):
+    can_read = RecordPermissionPolicy.can_read + [LibraryStaffIfArchives()]
+    can_read_files = RecordPermissionPolicy.can_read_files + [LibraryStaffIfArchives()]
