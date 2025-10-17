68b26f4a137470354ae185eea7f3661ed2f1aa55
phette23 <phette23@gmail.com>
2025-10-17 16:29:56 -0700

    fix: fill in Archives Series, closes #63

--- assets/templates/custom_fields/ArchivesSeries.js
+++ assets/templates/custom_fields/ArchivesSeries.js
@@ -4,22 +4,68 @@ import {Dropdown, FieldLabel, SelectField} from "react-invenio-forms"
 import {Grid} from "semantic-ui-react"
 
 // Archives Series is a hierarchical controlled vocab
-// selection of first one determines options for second one
-// TODO fill out vocab using CCA/C Archives Box List sheet
-// TODO https://github.com/cca/cca_invenio/issues/63
+// selection of first series determines subseries options
+// ! Sync with vault_miration/migrate/archives_series.json
 const archivesSeries = {
     "I. Administrative Materials": [
+        "1. Academic Council",
         "2. Accreditation and Licensing Materials",
-        "3. Board of Directors and Trustees",
-        "4. Faculty Assembly and Faculty Senate"
+        "3. Board of Directors",
+        "4. Faculty Assembly / Faculty Senate",
+        "5. Faculty and Staff Files",
+        "6. Finance and Development",
+        "7. General Admin Files",
+        "8. Partnerships and Affiliations",
+        "9. Presidents Office"
     ],
-    "II. Alumni Society Materials": ["2. Alumni Files"],
+    "II. Alumni Society Materials": [
+        "1. Alumni Files",
+        "2. Alumni Exhibitions"
+    ],
     "III. College Life": [
         "1. Commencement Materials",
         "2. Ephemera",
         "3. Events",
-        "5. Oral Histories",
+        "4. Awards"
     ],
+    "IV. Department Materials": [
+        "1. Academic Departments",
+        "2. Admin Departments",
+        "3. Other"
+    ],
+    "V. Exhibits and the CCA Collection": [
+        "1. Collection and Administration",
+        "2. Exhibits",
+        "3. Gallery Management Class"
+    ],
+    "VI. Photographs": [
+        "1. Commencement",
+        "2. Students",
+        "3. Alumni",
+        "4. Department/Studio",
+        "5. Events",
+        "6. Exhibits",
+        "7. Works of art",
+        "8. Photos by format"
+    ],
+    "VII. Press": [
+        "1. Press Clippings",
+        "2. Press Releases"
+    ],
+    "VIII. Periodicals and Other Publications": [
+        "1. College",
+        "2. Students",
+        "3. Alumni",
+        "4. Yearbooks",
+        "5. Issued an ISBN/ISSN",
+        "6. Catalogs/ Schedule of classes"
+    ],
+    "X. Buildings and Grounds": [
+        "1. Berkeley",
+        "2. Oakland",
+        "3. San Francisco",
+        "4. Other Locations"
+    ]
 }
 
 export class ArchivesSeries extends Component {
