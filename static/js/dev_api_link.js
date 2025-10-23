99de3b9e643f35a766abbe99e437a6da7a9f550f
phette23 <phette23@gmail.com>
2025-10-23 15:30:21 -0700

    add comment, minor fix to dev API link

--- static/js/dev_api_link.js
+++ static/js/dev_api_link.js
@@ -1,3 +1,4 @@
 // link to API representation of current page
-const html = `<div class="item"><a href="/api${location.pathname}?${location.search}">API</a></div>`
+const qs = location.search ? "?" + location.search : ""
+const html = `<div class="item"><a href="/api${location.pathname}${qs}">API</a></div>`
 $('#invenio-menu > .item').not('.right').last().after(html)
