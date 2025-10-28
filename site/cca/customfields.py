18f67a8b82485ad53e7f04c66981afe30446eef4
phette23 <phette23@gmail.com>
2025-10-28 14:41:24 -0700

    fix: rm demo program VocabularlyCF

--- site/cca/customfields.py
+++ site/cca/customfields.py
@@ -1,10 +1,7 @@
 from typing import Any
 
 from invenio_i18n import lazy_gettext as _
-from invenio_rdm_records.config import RDM_FACETS, RDM_SEARCH
 from invenio_records_resources.services.custom_fields import BaseCF, TextCF
-from invenio_records_resources.services.records.facets import CFTermsFacet
-from invenio_vocabularies.services.custom_fields import VocabularyCF
 from marshmallow import fields
 from marshmallow_utils.fields import SanitizedUnicode
 
@@ -24,13 +21,13 @@ class ArchivesSeriesCF(BaseCF):
 
     # BaseCF must implement field property
     @property
-    def field(self):
+    def field(self):  # type: ignore
         """Marshmallow field for custom fields."""
         return fields.Dict()
 
     # BaseCF must implement search mapping
     @property
-    def mapping(self) -> dict[str, Any]:
+    def mapping(self) -> dict[str, Any]:  # type: ignore
         """Return the mapping."""
         return {
             "properties": {
@@ -48,12 +45,6 @@ RDM_CUSTOM_FIELDS: list[BaseCF] = [
     ArchivesSeriesCF(name="cca:archives_series"),
     TextCF(name="cca:community_field"),
     TextCF(name="cca:conditional_field"),
-    VocabularyCF(  # the type of custom field, VocabularyCF is a controlled vocabulary
-        dump_options=True,  # True when the list of all possible values will be visible in the dropdown UI component, typically for small vocabularies
-        multiple=False,  # if the field accepts a list of values (True) or single value (False)
-        name="cca:program",  # name of the field, namespaced by `cca`
-        vocabulary_id="programs",  # controlled vocabulary id defined in the vocabularies.yaml file
-    ),
 ]
 
 RDM_CUSTOM_FIELDS_UI: list[dict[str, Any]] = [
@@ -83,21 +74,6 @@ RDM_CUSTOM_FIELDS_UI: list[dict[str, Any]] = [
                 },
             },
             {
-                "field": "cca:program",
-                "ui_widget": "AutocompleteDropdown",
-                "template": "program.html",
-                "props": {
-                    "autocompleteFrom": "/api/vocabularies/programs",
-                    "autocompleteFromAcceptHeader": "application/vnd.inveniordm.v1+json",
-                    "clearable": True,
-                    "description": _("Select one of CCA's academic programs"),
-                    "icon": "building",
-                    "label": _("Academic Program"),
-                    "multiple": False,
-                    "placeholder": _("Start typing to search..."),
-                },
-            },
-            {
                 "field": "cca:archives_series",
                 "template": "archivesseries.html",
                 "ui_widget": "ArchivesSeries",
@@ -121,21 +97,5 @@ RDM_CUSTOM_FIELDS_UI: list[dict[str, Any]] = [
     }
 ]
 
-RDM_FACETS: dict[str, dict[str, Any]] = {
-    **RDM_FACETS,
-    "program": {
-        "facet": CFTermsFacet(  # backend facet
-            field="cca:programs.id",  # id is the keyword field of a vocabulary
-            label="Academic Program",
-        ),
-        "ui": {  # ui display
-            "field": CFTermsFacet.field("cca:programs.id"),
-        },
-    },
-}
-
-# ! This facet does not show up on the search page.
-RDM_SEARCH: dict[str, Any] = {
-    **RDM_SEARCH,
-    "facets": RDM_SEARCH["facets"] + ["program"],
-}
+# TODO do we want to expose facets?
+# https://inveniordm.docs.cern.ch/operate/customize/metadata/custom_fields/records/#search
