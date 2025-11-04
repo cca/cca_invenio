de1de5a6fce686b4210049b2d2dd286d07ea5521
phette23 <phette23@gmail.com>
2025-11-04 15:42:20 -0800

    fix: cca site pkg version
    should update based on what's in root pyproject.toml

--- site/cca/__init__.py
+++ site/cca/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.0"
+# read and set version from main pyproject.toml
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__: str = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
