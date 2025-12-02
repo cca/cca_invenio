58ecbd4948c64feaa045673af1bb4ef8c830fe7b
phette23 <phette23@gmail.com>
2025-12-02 12:46:26 -0800

    fix: rm vocablist print statements

--- site/cca/vocablist/vocablist.py
+++ site/cca/vocablist/vocablist.py
@@ -1,5 +1,3 @@
-from pprint import PrettyPrinter
-
 from flask import render_template
 from flask.views import MethodView
 from invenio_vocabularies.records.models import VocabularyScheme, VocabularyType
@@ -13,9 +11,6 @@ class VocabListView(MethodView):
 
     def get(self):
         """Renders the template."""
-        pp = PrettyPrinter(indent=2)
         vocabs = VocabularyType.query.all()
         vocabs = vocabs + VocabularyScheme.query.all()
-        for v in vocabs:
-            pp.pprint(v)
         return render_template(self.template, vocabs=vocabs)
