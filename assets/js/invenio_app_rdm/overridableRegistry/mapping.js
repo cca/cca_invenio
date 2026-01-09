a2a637fbf0a610b19b6c25bd49ed3a754e09d79a
phette23 <phette23@gmail.com>
2026-01-08 16:10:24 -0800

    feat: Related Works in libs communities
    show the Related Works, aka related_identifiers, input field on the
    deposit form for a select list of Libraries-curated communities
    ref #83

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,12 +1,13 @@
 /**
- * Add here all the overridden components of your app.
+ * Override Invenio React Components https://inveniordm.docs.cern.ch/operate/customize/look-and-feel/override_components/
  * Example from Zenodo:
  * https://github.com/zenodo/zenodo-rdm/blob/master/assets/js/invenio_app_rdm/overridableRegistry/mapping.js#L3
- * Find the names of overridable components in RDMDepositForm.js:
- * https://github.com/inveniosoftware/invenio-app-rdm/blob/master/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
+ * Find the names of overridable components by running `reactOverridableEnableDevMode()` in dev tools
  * TODO we need to find a way to include the invenio_app_rdm default override (TimelineFeedHeader)
  * It's not in a JS package, is copying a bunch of files the only way?
  */
+
+import {RelatedWorksIfLibraries} from "../../components/LibrariesConditionalFields";
 
 export const overriddenComponents = {
     "InvenioAppRdm.Deposit.AccordionFieldFunding.container": () => null,
@@ -15,7 +16,7 @@ export const overriddenComponents = {
     "InvenioAppRdm.Deposit.PublisherField.container": () => null,
     "InvenioAppRdm.Deposit.AccordionFieldAlternateIdentifiers.container": () => null,
     // TODO look into conditional container that shows this for Libraries community records
-    "InvenioAppRdm.Deposit.AccordionFieldRelatedWorks.container": () => null,
+    "InvenioAppRdm.Deposit.AccordionFieldRelatedWorks.container": RelatedWorksIfLibraries,
     "InvenioAppRdm.Deposit.AccordionFieldReferences.container": () => null,
     "InvenioCommunities.CommunityProfileForm.AccordionField.MetadataFunding": () => null
 }
